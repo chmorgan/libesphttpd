bc9a33f6aba333e75b74085d854dba0f8e18ad2f
Paul Freund <freund.paul@lvl3.org>
2018-09-05 23:10:14 +0200

    Changed to pragma once style

--- core/libesphttpd_base64.h
+++ core/libesphttpd_base64.h
@@ -1,6 +1,4 @@
-#ifndef LIBESPHTTPD_BASE64_H
-#define LIBESPHTTPD_BASE64_H
+#pragma once
 
 int libesphttpd_base64_decode(size_t in_len, const char *in, size_t out_len, unsigned char *out);
 int libesphttpd_base64_encode(size_t in_len, const unsigned char *in, size_t out_len, char *out);
-#endif
